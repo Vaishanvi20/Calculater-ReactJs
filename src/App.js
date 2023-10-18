fb3f76dafd27a4ab85b717180925386652eec2af
Vaishnavi <115980995+Vaishanvi20@users.noreply.github.com>
2023-10-18 06:01:52 +0000

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [value, setValue]= useState('')
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="Container">
+      <div className="calculator">
+        <form action="">
+          <div className="display">
+           <input type="text"/>
+          </div>
+          <div>
+            <input type="button" value="AC" />
+            <input type="button" value="DE" />
+            <input type="button" value="." />
+            <input type="button" value="/" />
+          </div>
+          <div>
+            <input type="button" value="7" />
+            <input type="button" value="8" />
+            <input type="button" value="9" />
+            <input type="button" value="*" />
+          </div>
+          <div>
+            <input type="button" value="4" />
+            <input type="button" value="5" />
+            <input type="button" value="6" />
+            <input type="button" value="+" />
+          </div>
+          <div>
+            <input type="button" value="1" />
+            <input type="button" value="2" />
+            <input type="button" value="3" />
+            <input type="button" value="-" />
+          </div>
+          <div>
+            <input type="button" value="00" />
+            <input type="button" value="0" />
+            <input type="button" value="=" />
+            
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
